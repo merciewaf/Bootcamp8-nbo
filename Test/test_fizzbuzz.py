92994debb24ba1bafea4e93909b96b5ba96d6d5b
mercie wafula <mrcwafula@gmail.com>
2016-06-17 17:52:15 +0300

    Adding test for notes application

--- Test/test_fizzbuzz.py
+++ Test/test_fizzbuzz.py
@@ -6,8 +6,21 @@ class FizzBuzzTest(unittest.TestCase):
 	"""Testing FizzBuzzTest
 	"""
 
-	def test_returns_fizz_when_divisible_by_three(self):
-	    self.assertEqual(fizz_buzz(3), 'fizz')	
+	def test_fizz_1(self):
+        self.assertEqual(fizz_buzz(3) 'Fizz')
+    
+    def test_fizz_2(self):
+        self.assertEqual(fizz_buzz(33) 'Fizz')
+    
+    def test_buzz_1(self):
+        self.assertEqual(fizz_buzz(5)'Buzz')
+    
+    def test_buzz_2(self):
+        self.assertEqual(fizz_buzz(25), 'Buzz')
 
-	def test_returns_fizz_when_divisible_by_five(self):
-	    self.assertEqual(fizz_buzz(5), 'buzz')	
+    def test_fizz_buzz_1(self):
+        self.assertEqual(fizz_buzz(15), 'FizzBuzz')
+    
+    def test_fizz_buzz_2(self):
+        self.assertEqual(fizz_buzz(105), 'FizzBuzz')
+        
